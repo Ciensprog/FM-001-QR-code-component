7ea49824e3022ce1b88364dc50ed4c5c7df0f312
Ciensprog <jorg.leonrd@gmail.com>
2023-02-05 00:35:47 -0600

    Add initial structure for card

--- src/App.tsx
+++ src/App.tsx
@@ -5,5 +5,11 @@
 */
 
 export function App() {
-  return <>App</>
+  return (
+    <main>
+      <article className="bg-white p-4 rounded-[1.25rem] text-center w-80 max-w-full">
+        App
+      </article>
+    </main>
+  )
 }
