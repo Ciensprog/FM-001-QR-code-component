4d22627c48e10cb0584802882d41f84ee8f7a7d7
Ciensprog <jorg.leonrd@gmail.com>
2023-02-05 00:41:06 -0600

    Add QR Code

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,13 @@ export function App() {
   return (
     <main>
       <article className="bg-white p-4 rounded-[1.25rem] text-center w-80 max-w-full">
-        App
+        <picture>
+          <img
+            src="./qr-code.png"
+            className="rounded-[0.625rem]"
+            alt="QR Code"
+          />
+        </picture>
       </article>
     </main>
   )
