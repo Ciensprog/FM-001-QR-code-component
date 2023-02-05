35d679e37a830e8dcdfb925fbc5cc004ed4466fb
Ciensprog <jorg.leonrd@gmail.com>
2023-02-05 00:05:57 -0600

    Add Tailwindcss support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import { App } from './App'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+/*
+|-------------------------------------------------------------------------------
+| Render
+|-------------------------------------------------------------------------------
+*/
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
