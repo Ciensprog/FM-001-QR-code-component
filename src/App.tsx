6bf18f76fa5dbff31f5f8cd6ee82607e63d87726
Ciensprog <jorg.leonrd@gmail.com>
2023-02-05 00:50:03 -0600

    Add information

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@
 export function App() {
   return (
     <main>
-      <article className="bg-white p-4 rounded-[1.25rem] text-center w-80 max-w-full">
+      <article className="bg-white p-4 pb-10 rounded-[1.25rem] text-center w-80 max-w-full">
         <picture>
           <img
             src="./qr-code.png"
@@ -15,6 +15,15 @@ export function App() {
             alt="QR Code"
           />
         </picture>
+        <section className="px-4">
+          <h1 className="font-bold leading-7 mt-6 text-[1.375rem] text-dark-navy">
+            Improve your front-end skills by building projects
+          </h1>
+          <p className="mt-4 text-[0.9375rem] text-grey">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </section>
       </article>
     </main>
   )
