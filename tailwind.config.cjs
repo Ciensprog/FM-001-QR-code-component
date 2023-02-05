18836fdb648639ce67f007dc9066da39425b4bfe
Ciensprog <jorg.leonrd@gmail.com>
2023-02-05 00:29:26 -0600

    Add new styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+const colors = {
+  base: '#D5E1EF',
+  'dark-navy': '#1F314F',
+  grey: '#7D889E',
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 }
